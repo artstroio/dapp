419f0606fda3e091744ef0f233b154b985f24b8f
ARTSTRO <79707041+artstroio@users.noreply.github.com>
2021-03-27 00:01:12 -0400

    Revert "Update web3.js"
    This reverts commit b965e61e94403d7ae8366feac190f01c48a7bb11.

--- js/web3.js
+++ js/web3.js
@@ -1,7 +1,3 @@
-const web3 = new Web3('https://floral-rough-snow.bsc.quiknode.pro/');
-web3.eth.getBlock('latest').then(answer => console.log(answer))
-web3.eth.getBlockNumber().then(blockNum => console.log(blockNum))
-//UP
 const inputELm = document.querySelector("#thumbnail");
 const descriptionELm = document.querySelector("#description");
 const supplyELm = document.querySelector("#supply");
