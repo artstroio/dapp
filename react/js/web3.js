844f55f5d3c6d507dad6f38172617930890bd1e3
ARTSTRO <79707041+artstroio@users.noreply.github.com>
2021-04-13 10:33:59 -0400

    Update web3.js

--- react/js/web3.js
+++ react/js/web3.js
@@ -1144,10 +1144,7 @@ function findTokenInYouWallet_walletPage_individual(id) {
                   }
                   listaTokens += `
               <article id="show-token" class="wallet panel">
-                   <header>
-                      <h2>Show Token</h2>
-                    </header> 
-                      <small>This token is in your Wallet. If you want to sell this token, please take note of the Id number and send it to the Sell Contract clicking <a href="https://artstro.io/#send-token-sell"><span>HERE.</span></a> </small>                     
+  
               <section>
               <form action="#" method="post" onsubmit="return false">
                 </br>										
@@ -1220,13 +1217,15 @@ function findTokenInYouWallet_page_show_token(id) {
                 }
                 listaTokens += `
             <article id="show-token" class="wallet panel">
-
+                   <header>
+                      <h2>Show Token</h2>
+                    </header> 
+                      <small>This token is in your Wallet. If you want to sell this token, please take note of the Id number and send it to the Sell Contract clicking <a href="https://artstro.io/#send-token-sell"><span>HERE.</span></a> </small>
             <section>
             <form action="#" method="post" onsubmit="return false">
               </br>										
               <div class="row">	
-  
-                <div class="col-4 col-6-medium col-12-small imagen-token" style="margin-top:20px;">
+                <div class="col-6 col-12-medium imagen-token" style="margin-top:20px;">
                     <a target="_blank" href=${optionalLink} class="image fit">${tag}</a>
                 <h2>${des}</h2>
                 <p>Token Id: <a target="_blank" href="https://bscscan.com/token/${contractAddress}?a=${id}">${id}</a></p>
